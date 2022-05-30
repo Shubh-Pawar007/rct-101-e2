232fc0e097263a37ff8a10cc63172c6b9ac2014e
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-05-30 17:16:25 +0530

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
+import Product from "./Product";
+import { Grid, GridItem } from '@chakra-ui/react';
+import { Flex, Spacer } from '@chakra-ui/react'
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+  const [bag,setBag]=useState([])
+  
 
+  useEffect(()=>{
+    getData()
+  },[])
+
+  const getData=async()=>{
+    let res= await axios.get("http://localhost:8080/products");
+    let data=res.data;
+    
+    
+    console.log(data);
+    setBag(data);
+}
   return (
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      <AddProduct/>
+      <Grid>{/* List of Products */}
+        {bag.map((e)=>{
+          <Product key={e.id} product={e}/>
+        })}
+      </Grid>
       {/* Pagination */}
+      <Pagination />
     </Flex>
   );
 };
